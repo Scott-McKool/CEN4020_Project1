33e0de40e624272a500084d07f569686100a0c76
scott <scott262@usf.edu>
2026-02-04 14:27:14 -0500

    added level 2 game board

--- main.py
+++ main.py
@@ -2,51 +2,54 @@
 from json import load, dump
 from random import randint
 
-class Move_result:
-    '''The result of an attempted move on the game board, it is either a success or not, and has an acompanying decription'''
+class Result:
+    '''The result of attempting a given action, it is either successfull or not, and includes a text description of the result'''
     data: tuple[bool, str]
     def __init__(self, success: bool, description: str = "Description missing."):
         self.data = (success, description)
 
     def success(self) -> bool:
-        '''Was the move successfull'''
+        '''Does the result represent a success (true if yes, false if no)'''
         return self.data[0]
     
     def description(self) -> str:
-        '''The description of the move'''
+        '''The description of the result'''
         return self.data[1] or "Description missing."
-
+    
     def __str__(self):
         if self.success():
-            return self.description()
+            return f"Successful: {self.description()}"
         
         if not self.success():
-            return f"unsuccessfull move: {self.data[1]}"
+            return f"Unsuccessfull: {self.description()}"
+
 
 class Game:
     cells: list[list[int]]
     size : int
     score: int
     level: int
+    cur_move: int
     def __init__(self, size: int):
+        self.cur_move = 2
         self.level = None
-        self.score = 0
         self.size = size
+        self.score = 0
         # init cell values 
         self.cells = [[0]*size for _ in range(size)]
     
-    def place(self, x: int, y: int, value: int) -> Move_result:
+    def place(self, x: int, y: int, value: int) -> Result:
         '''Attempt to place a value at a given coordinate within the rules of the game. x and y are index values'''
         # make sure coords are in-bounds
         if x >= self.size or y >= self.size or x < 0 or y < 0:
-            return Move_result(False, "Given coordinates are out of bounds.")
+            return Result(False, "Given coordinates are out of bounds.")
         
         # make sure space in unnoccupied
         if self.cells[x][y] != 0:
-            return Move_result(False, "Space is already filled.")
+            return Result(False, "Space is already filled.")
         
         # ok to proceed
-        return Move_result(True)
+        return Result(True)
         
     def from_data(self, data: dict) -> None:
         '''Populate the object's atributes from a dictionary'''
@@ -54,6 +57,8 @@ class Game:
         self.size  = data["size"]
         self.score = data["score"]
         self.level = data["level"]
+        self.cur_move  = data["cur_move"]
+
 
     def __str__(self):
         result: str = ""
@@ -84,7 +89,6 @@ class Level1(Game):
         self.level: int = 1
 
         ### initialize board for play
-        self.cur_move: int = 2
         # choose location for '1'
         rand_x: int = randint(0, self.size - 1)
         rand_y: int = randint(0, self.size - 1)
@@ -92,7 +96,7 @@ class Level1(Game):
         self.last_move = (rand_x, rand_y)
 
 
-    def place(self, x: int, y: int, value: int = None) -> Move_result:
+    def place(self, x: int, y: int, value: int = None) -> Result:
         # does the placement pass the basic checks?
         basic_checks = super().place(x, y, value)
         if not basic_checks.success():
@@ -103,43 +107,104 @@ class Level1(Game):
         dy = abs(self.last_move[1] - y)
 
         if dx > 1 or dy > 1:
-            return Move_result(False, "Move must be a neighbor of its predececcor.")
+            return Result(False, "Move must be a neighbor of its predececcor.")
         
         # asses score for the move (is move place on a diagonal)
         if dx == dy:
             self.score += 1
 
-        # make the move
+        # make sure value is correct (ascending order)
         if not value:
             value = self.cur_move
             self.cur_move += 1
         elif value != self.cur_move:
-            return Move_result(False, "Move's value must be 1 greater than its predecesor.")
+            return Result(False, "Move's value must be 1 greater than its predecesor.")
 
+        # make the move
         self.cells[x][y] = value
         self.last_move = (x, y)
-        return Move_result(True)
+        return Result(True)
 
     def from_data(self, data) -> None:
-        self.cur_move  = data["cur_move"]
         self.last_move = data["last_move"]
         super().from_data(data)
 
 
+class Level2(Game):
+    def __init__(self, base_game: Level1):
+        # a level2 board is initialized from a level1 board, but with the outer ring of spaces
+        size = base_game.size + 2
+        super().__init__(size)
+        # inherit values from the level1 board
+        self.score = base_game.score
+        self.level = 2
+        # carry over the values from the level 1 board
+        for y in range(0, base_game.size):
+            for x in range(0, base_game.size):
+                self.cells[x+1][y+1] = base_game.cells[x][y]
+
+
+    def _search_in_line(self, x: int, y: int, dx: int, dy: int, value: int) -> bool:
+        '''Searches in a straight line, starting from cell (x,y), and moving according to (dx, dy), searching for value returns true if found, returns false if not.
+        \nx and y are index values'''
+        i: int = x
+        j: int = y
+        # while the search is in bounds
+        while i >= 0 and j >= 0 and i < self.size and j < self.size:
+            # found value
+            if self.cells[i][j] == value:
+                return True
+            i += dx
+            j += dy
+        # could not find value
+        return False
+
+
+    def place(self, x: int, y: int, value: int = None) -> Result:
+        # does the placement pass the basic checks?
+        basic_checks = super().place(x, y, value)
+        if not basic_checks.success():
+            return basic_checks
+
+        # do checks for if value is present in the apropriate line
+        found_value: bool
+        deltaX: int = 0
+        deltaY: int = 0
+        if x == 0:
+            deltaX = 1
+        if x == self.size - 1:
+            deltaX = -1
+        if y == 0:
+            deltaY = 1
+        if y == self.size - 1:
+            deltaY = -1
+        found_value = self._search_in_line(x, y, deltaX, deltaY, value)
+
+        if found_value == False:
+            return Result(False, "The number of the move and the number in the inner board must be on a straight line.")
+
+        # make the move
+        self.cells[x][y] = value
+        return Result(True)
+
+    def from_data(self, data) -> None:
+        super().from_data(data)
+
 class Game_loader():
     levels: dict = {
-        1 : Level1
+        1 : Level1,
+        2 : Level2
     }
         
     def save_game(game: Game, name: str):
         '''Saves a provided game object to a json text file with the provided name'''
-        with open(f"saved_games/{name}", "wt") as outfile:
+        with open(f"saved_games/{name}.json", "wt") as outfile:
             dump(game.__dict__, outfile, indent=4)
 
     def load_game(name: str) -> Game:
         '''loads a game board of the appropriate type from a json file'''
         obj: Game
-        with open(f"saved_games/{name}") as infile:
+        with open(f"saved_games/{name}.json") as infile:
             data = load(infile)
         
         for key, cls in Game_loader.levels.items():
@@ -153,7 +218,7 @@ class Game_loader():
 
 
 if __name__ == "__main__":
-    newGame: Game = Level1(10)
+    newGame: Game = Level1(5)
 
     while True:
 
@@ -173,8 +238,8 @@ if __name__ == "__main__":
             newGame = Game_loader.load_game(in_str)
             continue
 
-        x, y = in_str.split(" ")
-        did_place: Move_result = newGame.place(int(x)-1, int(y)-1)
+        x, y, val = in_str.split(" ")
+        did_place: Result = newGame.place(int(x)-1, int(y)-1, int(val))
 
         if not did_place.success():
             print(did_place)
